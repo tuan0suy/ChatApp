ea4b749a542c0923f26ac76b9556b9a36a6e664d
Tuan <tuanchu24112004@gmail.com>
2025-04-14 22:39:52 +0700

    Profile Image

--- app/src/main/java/com/example/chatapp/ChatActivity.java
+++ app/src/main/java/com/example/chatapp/ChatActivity.java
@@ -1,8 +1,10 @@
 package com.example.chatapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -36,6 +38,7 @@ public class ChatActivity extends AppCompatActivity {
     ImageButton backBtn;
     TextView otherUsername;
     RecyclerView recyclerView;
+    ImageView profilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +58,15 @@ public class ChatActivity extends AppCompatActivity {
         backBtn = findViewById(R.id.back_btn);
         otherUsername = findViewById(R.id.other_username);
         recyclerView = findViewById(R.id.chat_recycle_view);
+        profilePic = findViewById(R.id.profile_pic_image_view);
+
+        FirebaseUtil.getOtherUserProfilePicStorageReference(otherUser.getUserId()).getDownloadUrl()
+                .addOnCompleteListener(t -> {
+                    if (t.isSuccessful()) {
+                        Uri uri = t.getResult();
+                        AndroidUtil.setProfilePic(this, uri, profilePic);
+                    }
+                });
 
         backBtn.setOnClickListener(v -> {
             onBackPressed();
--- app/src/main/java/com/example/chatapp/adapter/RecentChatRecyclerAdapter.java
+++ app/src/main/java/com/example/chatapp/adapter/RecentChatRecyclerAdapter.java
@@ -2,6 +2,7 @@ package com.example.chatapp.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,6 +40,13 @@ public class RecentChatRecyclerAdapter extends FirestoreRecyclerAdapter<Chatroom
                 boolean lastMessageSentByMe = model.getLastMessageSenderId().equals(FirebaseUtil.currentUserId());
 
                 UserModel otherUserModel = task.getResult().toObject(UserModel.class);
+                FirebaseUtil.getOtherUserProfilePicStorageReference(otherUserModel.getUserId()).getDownloadUrl()
+                        .addOnCompleteListener(t -> {
+                            if (t.isSuccessful()) {
+                                Uri uri = t.getResult();
+                                AndroidUtil.setProfilePic(context, uri, holder.profilePic);
+                            }
+                        });
                 holder.usernameText.setText(otherUserModel.getUsername());
                 if (lastMessageSentByMe)
                     holder.lastMessageText.setText("You : "+model.getLastMessage());
